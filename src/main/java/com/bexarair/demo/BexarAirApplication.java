5513abe9dd1f855d6c6791afd19c02421b9a9b59
Jlampkins <Jlampkins42@gmail.com>
2019-05-15 12:28:21 -0500

    first commit

---
+++ src/main/java/com/bexarair/demo/BexarAirApplication.java
@@ -0,0 +1,13 @@
+package com.bexarair.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BexarAirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BexarAirApplication.class, args);
+    }
+
+}
