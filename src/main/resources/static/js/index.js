d456d38c114823ba0e462a50147d6ed785024771
Bethany Ramirez <bethany.e.ramirez@gmail.com>
2019-05-16 09:25:02 -0500

    container built to store map

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     "use strict";
 
-    var map;
+    var map, service;
     // function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: -34.397, lng: 150.644},
@@ -15,22 +15,53 @@ $(document).ready(function() {
                 lat: place.geometry.location.lat(),
                 lng: place.geometry.location.lng()
             };
-            var marker = new google.maps.Marker({
-                position: pos,
-                map: map
-            });
-
-
-            marker.addListener('click', function () {
-                infoWindow.open(map, marker);
-            })
 
 
         }
 
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            createInfoWindow(pos);
+            map.setCenter(pos);
+            console.log(map.getCenter());
+            var request = {
+                location: pos,
+                radius: 500,
+                type: ['bar']
+            };
 
-        // console.log(infoWindow.getPosition());
-    // } // end of initialize map function
+            service = new google.maps.places.PlacesService(map);
+            service.textSearch(request, callback);
+
+            function callback(results, status) {
+                console.log(results);
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    for (var i = 0; i < results.length; i++) {
+                        var place = results[i];
+                        createMarker(place);
+                    }
+                }
+            }
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+    }
 
 
 
