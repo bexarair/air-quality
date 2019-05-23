3e998513c757ca2b574a9cb560116671fd3938ab
Anwar Lopez <anwarlo247@gmail.com>
2019-05-23 11:29:16 -0500

    added db file repo file

--- src/main/java/com/bexarair/demo/repositories/DBFileRepository.java
+++ src/main/java/com/bexarair/demo/repositories/DBFileRepository.java
@@ -1,4 +1,10 @@
 package com.bexarair.demo.repositories;
 
-public class DBFileRepository {
+import com.bexarair.demo.models.DBFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DBFileRepository extends JpaRepository<DBFile, String> {
+
 }
