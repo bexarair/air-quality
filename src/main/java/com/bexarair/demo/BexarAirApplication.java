eb7ad07306e5d101bda129fe1e3202497d5f284a
Anwar Lopez <anwarlo247@gmail.com>
2019-05-26 23:05:11 -0500

    set to upload to local directory

--- src/main/java/com/bexarair/demo/BexarAirApplication.java
+++ src/main/java/com/bexarair/demo/BexarAirApplication.java
@@ -1,15 +1,23 @@
 package com.bexarair.demo;
 
+import com.bexarair.demo.controllers.FileController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.io.File;
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableScheduling
 public class BexarAirApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        new File(FileController.uploadingDir).mkdirs();
         SpringApplication.run(BexarAirApplication.class, args);
+        String currentDirectory = System.getProperty("user.dir");
+        System.out.println("user.dir: " + currentDirectory);
     }
+
 
 }
