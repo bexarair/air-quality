5ad3d9267ae19a36b3719a2c5bcdd83311767719
Jlampkins <Jlampkins42@gmail.com>
2019-05-30 09:53:42 -0500

    promise issues

--- src/main/java/com/bexarair/demo/controllers/AirQualityController.java
+++ src/main/java/com/bexarair/demo/controllers/AirQualityController.java
@@ -44,7 +44,7 @@ public class AirQualityController {
     private static final LocalDate tomorrowDate = LocalDate.now().plus(1, ChronoUnit.DAYS);
     private static final String dateURL = "&date=" + tomorrowDate;
     private static final String distanceURL = "&distance=0&API_KEY=";
-    private static final String apiKey = "C9616B67-963C-4F6A-A5B5-C81979B9FF27";
+    private static final String apiKey = "A4D00993-8E59-4B13-924E-9BA79D1FCE63";
     private static final String[] zipcodes = {"78002","78006","78009","78015","78023","78039","78052","78056","78063","78064","78065","78066","78069","78073","78101","78108","78109","78112","78114","78121","78124","78148","78150","78152","78154","78155","78163","78201","78202","78203","78204","78205","78207","78208","78209","78210","78211","78212","78213","78214","78215","78216","78217","78218","78219","78220","78221","78222","78223","78224","78225","78226","78227","78228","78229","78230","78231","78232","78233","78234","78235","78236","78237","78238","78239","78240","78242","78244","78245","78247","78248","78249","78250","78251","78252","78253","78254","78255","78256","78257","78258","78259","78260","78261","78263","78264","78266"};
     private static final String[] testZip = {"78002", "78006", "78009", "78015", "78023"};
     private static final String oneZip = "78002";
