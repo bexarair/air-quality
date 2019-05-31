ab4c8548d9264c65011834a7493bf97afa265365
Jlampkins <Jlampkins42@gmail.com>
2019-05-30 20:27:47 -0500

    cronomuim

--- src/main/java/com/bexarair/demo/controllers/AirQualityController.java
+++ src/main/java/com/bexarair/demo/controllers/AirQualityController.java
@@ -76,8 +76,8 @@ public class AirQualityController {
 
 
 //    @Scheduled(cron = "0 0 7 * * ?") //grab at 7am everyday
-    @Scheduled(cron = "0 25 21 * * ?") //grab at 7am everyday
-//    @Scheduled(fixedRate = 60000)
+//    @Scheduled(cron = "0 25 21 * * ?") //grab at 7am everyday
+    @Scheduled(fixedRate = 60000)
     public void getAir() {
         System.out.println("This is being ran");
         try {
